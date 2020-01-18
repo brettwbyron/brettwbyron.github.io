8918c35eba0fa59e61ac632e015143a3d06ed771
Brett Byron <brett.byron@jobvite-inc.com>
2020-01-18 16:55:42 -0500

    Adding animoji hover first draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 $( document ).on( 'ready', () => {
+    smoothScroll();
+    animoji();
+} );
+
+function smoothScroll() {
     // Smooth scroll
     $( 'a[href*="#"]:not([href="#"])' ).click( function () {
         if ( location.pathname.replace( /^\//, '' ) == this.pathname.replace( /^\//, '' ) ||
@@ -16,4 +21,14 @@ $( document ).on( 'ready', () => {
             }
         }
     } );
-} );
+}
+
+function animoji() {
+    $('.animoji').on('hover', () => {
+        $('.animoji').css('background-image','url("../images/animoji.gif")');
+    });
+
+    $('.animation').on('mouseout', () => {
+        $('.animoji').css('background-image','url("../images/animoji_reverse.gif")');
+    });
+}
