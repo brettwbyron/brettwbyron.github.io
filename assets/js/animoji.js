f49d99665d903c026c0fb8dcb43a481620fd149a
Brett Byron <brett.byron@jobvite-inc.com>
2020-01-20 11:01:20 -0500

    Adding animoji

---
+++ assets/js/animoji.js
@@ -0,0 +1,49 @@
+$(document).on('ready', function() {
+    var frames = 20;
+    var position;
+
+    for (var i = 1; i < frames; i++) {
+        var frame = document.createElement('div');
+        frame.id = i;
+        frame.classList.add('animoji-frame');
+        frame.style.backgroundImage = "url('https://brettwbyron.github.io/assets/images/animoji/" + i + ".png'";
+        if (i = 1) { frame.style.opacity = '1' }
+        $('#animoji').append(frame);
+    }
+
+    // animoji hover
+    var interval;
+
+    $('#animoji').on( "mouseenter touchstart", ( e ) => {
+        clearInterval(interval);
+        setTimeout( () => {
+            position = 1;
+            interval = setInterval( () => {
+                var el = $('#' + position);
+
+                if (position > frames) {
+                    clearInterval(interval);
+                } else {
+                    if (position > 20) {position = 20;}
+                    el.css('opacity','1');
+                    el.css("backgroundImage", "https://brettwbyron.github.io/assets/images/animoji/" + i + ".png");
+                    position++;
+                }
+            }, 10 );
+        }, 100 );
+    } );
+    $('#animoji').on( "mouseout touchend", ( e ) => {
+        clearInterval(interval);
+        interval = setInterval( () => {
+            position--;
+            var el = $('#' + position);
+
+            if (position === 1) {
+                clearInterval(interval);
+            } else {
+                if (position > 20) {position = 20;}
+                el.css('opacity','0');
+            }
+        }, 10 );
+    } );
+});
